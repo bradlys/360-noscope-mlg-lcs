ae8a48d25124f71ff5f7cce3f053852ed4aa5fe8
Bradly Schlenker <bradlyschlenker@gmail.com>
2014-11-23 07:29:50 -0700

    Update get-rekt-m8.c
    Trivial documentation update. No standard documentation protocol here was used. The documentation is mostly just talking off the top of my head about space and time complexity. That's what this file is about anyway. It's not about being used in projects, it's a demonstration.

--- src/get-rekt-m8.c
+++ src/get-rekt-m8.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * O(N*M) time complexity method where
+ * N is length of A and M is length of B.
+ * This function returns the length
+ * of the longest common subsequence between
+ * A and B. It has a space complexity of
+ * O(2*M) (2 times M) but could be reduced to
+ * O(M+1). (M plus 1) Reason I didn't do that is
+ * 'cause I implemented that solution earlier
+ * and then erased the god damn thing without
+ * saving it.
+ * 
+ * I'm not sure but I think it might also be possible to reduce
+ * the space complexity to O(min(N, M) + 1) for this method.
+ * Set K[min(N,M)] and iterate appropriately.
+ *
+ */
 int getLCSLength(long long int * A, int Abegin, int Aend, long long int * B, int Bbegin, int Bend){
     int Alen = Aend - Abegin;
     int Blen = Bend - Bbegin;
@@ -36,6 +53,24 @@ int getLCSLength(long long int * A, int Abegin, int Aend, long long int * B, int
     return K[1][Blen];
 }
 
+/**
+ * Prints the longest common subsequence
+ * to standard out. This will run 
+ * approximately O(log(N+M)) times due to
+ * recursive calls. Fortunately, this uses
+ * very little space. Of course, it's not
+ * a constant amount of space. It's something like
+ * O(log(M+N)). Which in the scheme of things, ain't bad.
+ * The reason it uses that space is because of all the recursive
+ * calls (O(log(M+N)) and the storage of things like
+ * Alen, Blen, AAbegin, etc.
+ * 
+ * And then, of course, it runs getLCSLength() which takes a
+ * monsterous O(M*N) time. It runs it a lot too. If space
+ * were not a huge issue then possibly having a lookup table
+ * for most frequently called getLCSLength() values could
+ * speed up time. Just a thought. Memoization is a thing.
+ */
 void findLCS(long long int * A, int Abegin, int Aend, long long int * B, int Bbegin, int Bend){
     int Alen = Aend - Abegin;
     int Blen = Bend - Bbegin;
@@ -84,12 +119,21 @@ void findLCS(long long int * A, int Abegin, int Aend, long long int * B, int Bbe
     }
 }
 
+/**
+ * Takes the data from standard in,
+ * outputs it to standard out. What more could you
+ * ever ask for?
+ * 
+ * Not including constant factors:
+ * Approx Runtime: O(M*N*log(N+M))
+ * Approx Space: O(M+N)
+ * Approx rekt: O(MLG)
+ * Approx chance of hiring manger taking this lightly: 0%
+ */
 int main() {
-    int n = 0;
-    int m = 0;
+    int n = 0; int m = 0;
     scanf("%i", &n); scanf("%i", &m);
-    long long int A[n];
-    long long int B[m];
+    long long int A[n]; long long int B[m];
     for(int i = 0; i < n; i++){
         scanf("%lli", &A[i]);
     }
