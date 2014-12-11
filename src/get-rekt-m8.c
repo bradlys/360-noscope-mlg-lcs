2f7bf2b061331cd47f4cf3b09a3665987542f530
Bradly Schlenker <bradlyschlenker@gmail.com>
2014-12-11 13:36:36 -0800

    Few updates
    Minimized space requirement from O(2*M+1) to O(M+1) in getLCSLength. Unfortunately, I could not obtain the holy grail of O(MIN(N,M) + 1). For some reason, it was failing my test cases.
    Which brings me to the next thing: I've included some sample test cases. Input with expected output.
    Added/modified documentation as well.

--- src/get-rekt-m8.c
+++ src/get-rekt-m8.c
@@ -6,16 +6,13 @@
  * N is length of A and M is length of B.
  * This function returns the length
  * of the longest common subsequence between
- * A and B. It has a space complexity of
- * O(2*M) (2 times M) but could be reduced to
- * O(M+1). (M plus 1) Reason I didn't do that is
- * 'cause I implemented that solution earlier
- * and then erased the god damn thing without
- * saving it.
+ * A and B. This has O(M+1) space complexity outside
+ * supplied argument strings O(M+N). Altogether,
+ * it is linear space.
  * 
- * I'm not sure but I think it might also be possible to reduce
- * the space complexity to O(min(N, M) + 1) for this method.
- * Set K[min(N,M)] and iterate appropriately.
+ * There should be a methodology to improve the
+ * space complexity to O(MIN(N,M) + 1) but I could
+ * not implement it and have it function correctly.
  *
  */
 int getLCSLength(long long int * A, int Abegin, int Aend, long long int * B, int Bbegin, int Bend){
@@ -25,64 +22,73 @@ int getLCSLength(long long int * A, int Abegin, int Aend, long long int * B, int
         return 0;
     }
     long long int * newA = A;
-    long long int * newB = B;
-    newA += Abegin;
-    newB += Bbegin;
-    int K[2][Blen+1];
-    for(int i = 0; i < Blen+1; i++){
-        K[1][i] = 0;
-        K[0][i] = 0;
+	long long int * newB = B;
+	newA += Abegin;
+	newB += Bbegin;
+    int K[Blen];
+    int i = 0;
+    //Make sure K array is all zeroes
+    for(i = 0; i < Blen; i++){
+        K[i] = 0;
     }
-    for(int i = 0; i < Alen; i++){
-        for(int j = 0; j < Blen+1; j++){
-            K[0][j] = K[1][j];
-        }
-        K[1][0] = 0;
-        for(int j = 1; j < Blen+1; j++){
-            if(newA[i] == newB[j-1]){
-                K[1][j] = K[0][j-1] + 1;
+    //loop over rows
+    for(i = 0; i < Alen; i++){
+    	int j = 0;
+    	//initial column before letters is always 0
+    	long long int upperLeftCornerValue = 0;
+    	long long int leftValue = 0;
+    	//this is technically K[j] but it gets assigned later
+    	long long int aboveValue = 0;
+    	//Loop over columns
+        for(j = 0; j < Blen; j++){
+        	//if match, upper left column value + 1
+            if(newA[i] == newB[j]){
+                K[j] = upperLeftCornerValue + 1;
             } else {
-                if(K[0][j] > K[1][j-1]){
-                    K[1][j] = K[0][j];
-                } else {
-                    K[1][j] = K[1][j-1];
-                }
+            	//if j == 0, leftValue is always 0
+            	if(j > 0){
+            		leftValue = K[j-1];
+        		}
+        		//aboveValue is always the last rows value,
+        		//which is still in the array because K[j] hasn't
+        		//been replaced yet
+        		aboveValue = K[j];
+        		//assign K[j] whichever is larger
+            	if(leftValue > aboveValue){
+            		K[j] = leftValue;
+        		} else {
+        			K[j] = aboveValue;
+    			}
             }
+            upperLeftCornerValue = K[j];
         }
     }
-    return K[1][Blen];
+    return K[Blen-1];
 }
 
 /**
- * Prints the longest common subsequence
- * to standard out. This will run 
- * approximately O(log(N+M)) times due to
- * recursive calls. Fortunately, this uses
- * very little space. Of course, it's not
- * a constant amount of space. It's something like
- * O(log(M+N)). Which in the scheme of things, ain't bad.
- * The reason it uses that space is because of all the recursive
- * calls (O(log(M+N)) and the storage of things like
- * Alen, Blen, AAbegin, etc.
+ * Prints the longest common subsequence of two
+ * long long int arrays to standard out. Easily
+ * modified to return it in an array instead.
+ * Approximate runtime: O(M*N*(M+N))
+ * Approximate space: O(M+N)
  * 
- * And then, of course, it runs getLCSLength() which takes a
- * monsterous O(M*N) time. It runs it a lot too. If space
- * were not a huge issue then possibly having a lookup table
- * for most frequently called getLCSLength() values could
- * speed up time. Just a thought. Memoization is a thing.
  */
 void findLCS(long long int * A, int Abegin, int Aend, long long int * B, int Bbegin, int Bend){
     int Alen = Aend - Abegin;
+    if (Alen == 0){
+        return;
+    }
     int Blen = Bend - Bbegin;
     long long int * newA = A;
     long long int * newB = B;
     newA += Abegin;
     newB += Bbegin;
-    if (Alen == 0){
-        return;
-    } else if ( Alen == 1){
-        for(int i = 0; i < Blen; i++){
+    if ( Alen == 1){
+		int i = 0;
+        for(i = 0; i < Blen; i++){
             if(newA[0] == newB[i]){
+            	//bingo, throw it to standard out
                 printf("%lli ", newA[0]);
                 break;
             }
@@ -100,18 +106,22 @@ void findLCS(long long int * A, int Abegin, int Aend, long long int * B, int Bbe
         int BBbegin = Bbegin + Blen/2;
         int BAend = Bbegin + Blen/2;
         int BBend = Bbegin + Blen;
+        
         //Find correct split for B
         int sizeLCSOriginal = getLCSLength(A, Abegin, Aend, B, Bbegin, Bend);
-        for (int k = 0; k < Blen+1; k++){
+		int k = 0;
+        for (k = 0; k < Blen+1; k++){
             BBbegin = Bbegin + k;
             BAend = Bbegin + k;
             int sizeLCSA = getLCSLength(A, AAbegin, AAend, B, BAbegin, BAend);
             int sizeLCSB = getLCSLength(A, ABbegin, ABend, B, BBbegin, BBend);
             if(sizeLCSA + sizeLCSB == sizeLCSOriginal){
                 break;
+                //Found the split
             }
         }
         
+        //Let's split!
         findLCS(A, AAbegin, AAend, B, BAbegin, BAend);
         findLCS(A, ABbegin, ABend, B, BBbegin, BBend);
         
@@ -125,7 +135,7 @@ void findLCS(long long int * A, int Abegin, int Aend, long long int * B, int Bbe
  * ever ask for?
  * 
  * Not including constant factors:
- * Approx Runtime: O(M*N*log(N+M))
+ * Approx Runtime: O(M*N*(M+N))
  * Approx Space: O(M+N)
  * Approx rekt: O(MLG)
  * Approx chance of hiring manager taking this lightly: 0%
@@ -134,10 +144,11 @@ int main() {
     int n = 0; int m = 0;
     scanf("%i", &n); scanf("%i", &m);
     long long int A[n]; long long int B[m];
-    for(int i = 0; i < n; i++){
+    int i = 0;
+    for(i = 0; i < n; i++){
         scanf("%lli", &A[i]);
     }
-    for(int i = 0; i < m; i++){
+    for(i = 0; i < m; i++){
         scanf("%lli", &B[i]);
     }
     findLCS(A, 0, n, B, 0, m);
